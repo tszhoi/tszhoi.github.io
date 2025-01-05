d98a3ff210f6f76ddd0e86111d045d99fdb681b2
Tsz Hoi Lee​⬤ <tszhoi@jupitrr.com>
2025-01-04 19:53:26 -0800

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",               // All HTML files in the root directory
+    "./src/**/*.{js,ts}",     // Any JS or TS files in the `src` folder
+    "./components/**/*.html", // Add paths for other directories if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
